7605e19753a45b322ccc390915ac1ecfef3eb091
sajad <sajadkamali017@gmail.com>
2023-06-30 04:06:42 +0330

    feat: final commit

---
+++ README.md
@@ -0,0 +1,48 @@
+# Film Generator Life's Photo
+
+Film Generator Life's Photo is a Python script that generates a video by composing images from a directory. The script uses the MediaPipe library to detect facial landmarks and aligns the images based on the location of the eyes.
+
+## Prerequisites
+- Python 3.x
+- OpenCV (`cv2`) library
+- NumPy library
+- MediaPipe library
+
+## Installation
+1. Clone the repository or download the `film_generator_lifes_photo.py` file.
+2. Install the required dependencies by running the following command:
+   ```
+   pip install opencv-python numpy mediapipe
+   ```
+
+## Usage
+1. Place the images you want to include in the video in a directory.
+2. Update the `img_dir` variable in the script with the path to the image directory.
+3. Adjust the parameters according to your preferences:
+   - `left_eye_loc_std`: Specifies the standard location of the left eye (x, y) for alignment. Adjust this if the alignment is incorrect.
+   - `frame_rate`: Specifies the frame rate of the generated video.
+   - `frame_size`: Specifies the frame size (width, height) of the generated video.
+4. Run the script using the following command:
+   ```
+   python film_generator_lifes_photo.py
+   ```
+5. The script will process the images, align them based on the location of the eyes, and generate a video named `output.mp4` in the current directory.
+
+Note: Make sure the images in the directory have consistent aspect ratios for better alignment results.
+
+## Example 1
+
+![sample_1](https://github.com/sajiniho07/ImageBackGroundRemover/blob/master/output.mp4)
+
+## Acknowledgements
+- [OpenCV](https://opencv.org/): Open Source Computer Vision Library
+- [NumPy](https://numpy.org/): Fundamental package for scientific computing with Python
+- [MediaPipe](https://mediapipe.dev/): Cross-platform framework for building multimodal applied machine learning pipelines
+
+## License
+
+Made with :heart: by <a href="https://github.com/sajiniho07" target="_blank">Sajad Kamali</a>
+
+&#xa0;
+
+<a href="#top">Back to top</a>
--- film-generator-life-photo.py
+++ film-generator-life-photo.py
@@ -10,7 +10,7 @@ class FilmGeneratorLifesPhoto():
                                                 min_detection_confidence=0.5,
                                                 min_tracking_confidence=0.5)
         self.img_dir = "res"
-        self.delta_x_std = 20
+        self.left_eye_loc_std = [300, 150]
         self.frame_rate = 1
         self.frame_size = (640, 480)
         self.bg_width = self.frame_size[0]
@@ -19,28 +19,67 @@ class FilmGeneratorLifesPhoto():
 
 
     def release_video(self):
-        img_files = os.listdir(self.img_dir)
-        img_files.sort()
+        img_files = sorted(os.listdir(self.img_dir))
 
-        for image in img_files:
-            img_file = os.path.join(self.img_dir, image)
+        for img_name in img_files:
+            img_file = os.path.join(self.img_dir, img_name)
             img = cv2.imread(img_file)
             img_height, img_width, _ = img.shape
-            
-            eyes_loc = self.get_eyes_loc(img, img_height, img_width)
-            if eyes_loc is not None and len(eyes_loc) == 2:
-                delta_x = abs(eyes_loc[1][0] - eyes_loc[0][0])
 
-            white_image = np.full((self.bg_height, self.bg_width, 3), 200, dtype=np.uint8)
-            offset_x = (self.bg_width - img_width) // 2
-            offset_y = (self.bg_height - img_height) // 2
-            white_image[offset_y:offset_y+img_height, offset_x:offset_x+img_width] = img
+            eyes_loc = self.get_eyes_loc(img, img_height, img_width)
+            offset_x = self.left_eye_loc_std[0] - eyes_loc[0][0]
+            offset_y = self.left_eye_loc_std[1] - eyes_loc[0][1]
+
+            resized_image, offset_x, offset_y = self.resize_image(img, offset_x, offset_y)
             
-            self.video_writer.write(white_image)
+            img_height, img_width, _ = resized_image.shape
+            new_width = offset_x + img_width
+            new_height = offset_y + img_height
+            
+            try:
+                white_image = np.full((self.bg_height, self.bg_width, 3), 200, dtype=np.uint8)
+                white_image[offset_y:new_height, offset_x:new_width] = resized_image
+                self.video_writer.write(white_image)
+            except:
+                print(f'Couldnt write this file: {img_name}')
+                pass
 
         self.video_writer.release()
         cv2.destroyAllWindows()
 
+    def resize_image(self, img, offset_x, offset_y):
+        img_height, img_width, _ = img.shape
+        new_width = offset_x + img_width
+        new_height = offset_y + img_height
+
+        if new_height > self.bg_height and new_width > self.bg_width:
+            diff_w = new_width - self.bg_width
+            diff_h = new_height - self.bg_height
+            new_size = (img_width - diff_w, img_height - diff_h)
+            resized_image = cv2.resize(img, new_size)
+
+        elif new_width > self.bg_width:
+            diff_w = new_width - self.bg_width
+            new_size = (img_width - diff_w, img_height - diff_w)
+            resized_image = cv2.resize(img, new_size)
+
+        elif new_height > self.bg_height:
+            diff_h = new_height - self.bg_height
+            new_size = (img_width - diff_h, img_height - diff_h)
+            resized_image = cv2.resize(img, new_size)
+
+        else:
+            resized_image = img.copy()
+
+        if offset_x < 0:
+            resized_image = resized_image[:, abs(offset_x):img_width]
+            offset_x = 0
+        if offset_y < 0:
+            resized_image = resized_image[abs(offset_y):img_height, :]
+            offset_y = 0
+
+        return resized_image, offset_x, offset_y
+    
     def get_eyes_loc(self, img, img_height, img_width):
         img_rgb = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
         lms = self.model.process(img_rgb).multi_face_landmarks
@@ -52,7 +91,7 @@ class FilmGeneratorLifesPhoto():
                     if landmark_index in (463, 243):
                         lm0 = point.x, point.y
                         eyes_loc.append((int(lm0[0]*img_width), int(lm0[1]*img_height)))
-                        cv2.circle(img, (int(lm0[0]*img_width), int(lm0[1]*img_height)), 3, (0, 0, 255), cv2.FILLED)
+                        # cv2.circle(img, (int(lm0[0]*img_width), int(lm0[1]*img_height)), 3, (0, 0, 255), cv2.FILLED)
                     landmark_index += 1
         return eyes_loc
 
